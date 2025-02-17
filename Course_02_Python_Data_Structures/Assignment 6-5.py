eca4a2d14747f8a4831b1afe5521558f988d68fb
sultanul-ovi <sultanulislamovi@gmail.com>
2025-02-16 20:10:35 -0500

    chapter6

--- Course_02_Python_Data_Structures/Assignment 6-5.py
+++
@@ -1,5 +0,0 @@
-text = "X-DSPAM-Confidence:    0.8475";
-startPos = text.find(':')
-piece = text[startPos+1:]
-end = float(piece)
-print(end)
--- Course_02_Python_Data_Structures/assignment06_5.py
+++
@@ -1,5 +0,0 @@
-text = "X-DSPAM-Confidence:    0.8475"
-numpos = text.find('0')
-estr = text[numpos:]
-fnum = float(estr)
-print(fnum)
