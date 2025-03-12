4403b853a8f17b1b6d2d981ad141c7d714e30055
sultanul-ovi <sultanulislamovi@gmail.com>
2025-03-12 03:06:04 -0400

    fix

--- Course_01_Programming_for_Everybody/Assignment_chapter02.py
+++ Course_01_Programming_for_Everybody/Assignment_chapter02.py
@@ -13,7 +13,6 @@ program (the pay should be 96.25). You should use input to read a string and
 float() to convert the string to a number. Do not worry about error checking
 or bad user data.
 '''
-# This first line is provided for you
 
 hrs = input("Enter Hours:")
 rate = input("Enter Rate:")
