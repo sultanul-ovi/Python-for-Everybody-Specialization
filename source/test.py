26fa3c7a7c4e6778f3291f4ad43307dfc71980da
sultanul-ovi <sultanulislamovi@gmail.com>
2025-03-12 03:22:34 -0400

    c 1 done

--- source/test.py
+++ source/test.py
@@ -1,1 +1,23 @@
-print("My name is Ovi")
+largest = None
+smallest = None
+while True:
+    num = input('Enter a number: ')
+    if num == 'done' :
+        break
+    try:
+        number = float(num)
+    except:
+        print('Invalid input')
+        continue
+    if largest is None:
+        largest = number
+    elif largest < number:
+        largest = number
+    if smallest is None:
+        smallest = number
+    elif smallest > number:
+        smallest = number
+
+
+print("Maximum is", int(largest))
+print("Minimum is", int(smallest))
